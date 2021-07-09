4f6f9fc4f92dec307bdff89516993559849f2be1
Juhani Hämäläinen <jushii@gmail.com>
2021-07-09 11:50:50 +0300

    5.7.

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} by {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 5,
+    paddingBottom: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    maxWidth: '50%',
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} by {blog.author}{' '}
+      <button
+        onClick={() => {
+          setDetailsVisible(!detailsVisible)
+        }}
+      >
+        {detailsVisible ? 'hide' : 'view'}
+      </button>
+      {detailsVisible ? (
+        <div>
+          {blog.url}
+          <br></br>
+          likes {blog.likes} <button>like</button>
+          <br></br>
+          added by {blog.user.name}
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
+  )
+}
 
 export default Blog
