f180291967d6882f947b0239f5cef497dd83e180
hamaju <jushii@gmail.com>
2022-05-29 00:05:17 +0300

    update deps, add .prettierrc and switch to restcountries.com api

--- osa2/maiden-tietot/src/App.js
+++ osa2/maiden-tietot/src/App.js
@@ -9,12 +9,11 @@ const App = () => {
   const [weatherData, setWeatherData] = useState(null)
 
   useEffect(() => {
-    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+    axios.get('https://restcountries.com/v2/all').then((response) => {
       if (filter !== '') {
         const results = response.data.filter((country) =>
           country.name.toLowerCase().includes(filter.toLocaleLowerCase())
         )
-
         setCountries(results)
       }
     })
@@ -29,14 +28,14 @@ const App = () => {
         .get(
           `http://api.openweathermap.org/data/2.5/weather?q=${capital}&units=metric&appid=${api_key}`
         )
-        .then((res) => {
-          setWeatherData(res.data)
+        .then(({ data }) => {
+          setWeatherData(data)
         })
     }
   }, [countries])
 
-  const handleFilterChange = (e) => {
-    setFilter(e.target.value.toLowerCase())
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value.toLowerCase())
   }
 
   const handleCountryClick = (countryToShow) => {
