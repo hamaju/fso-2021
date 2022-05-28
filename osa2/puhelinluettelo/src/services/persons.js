288e0a499b0f71000107db575ec265e376ef5cd6
hamaju <jushii@gmail.com>
2022-05-29 00:12:07 +0300

    small refactor, update deps and add .prettierrc

--- osa2/puhelinluettelo/src/services/persons.js
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -3,23 +3,23 @@ import axios from 'axios'
 const baseUrl = '/api/persons'
 
 const getAll = () => {
-  const req = axios.get(baseUrl)
-  return req.then((res) => res.data)
+  const request = axios.get(baseUrl)
+  return request.then((response) => response.data)
 }
 
 const create = (newObject) => {
-  const req = axios.post(baseUrl, newObject)
-  return req.then((res) => res.data)
+  const request = axios.post(baseUrl, newObject)
+  return request.then((response) => response.data)
 }
 
 const update = (id, newObject) => {
-  const req = axios.put(`${baseUrl}/${id}`, newObject)
-  return req.then((res) => res.data)
+  const request = axios.put(`${baseUrl}/${id}`, newObject)
+  return request.then((response) => response.data)
 }
 
 const remove = (id) => {
-  const req = axios.delete(`${baseUrl}/${id}`)
-  return req.then((res) => res.data)
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
 }
 
 const personService = {
