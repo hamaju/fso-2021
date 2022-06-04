6146ab9ae658fb2985dc677ad576214bc20469f5
hamaju <jushii@gmail.com>
2022-06-05 00:42:11 +0300

    update react and react-dom

--- part9/patientor/src/index.tsx
+++ part9/patientor/src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "semantic-ui-css/semantic.min.css";
 import App from "./App";
 import { reducer, StateProvider } from "./state";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <StateProvider reducer={reducer}>
     <App />
-  </StateProvider>,
-  document.getElementById("root")
+  </StateProvider>
 );
